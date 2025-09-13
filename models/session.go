b356307dca173d75ce8380e7e06b3874885f06e2
YouROK <8yourok8@mail.ru>
2025-09-13 12:13:17 +0300

    add expired sessions

--- models/session.go
+++ models/session.go
@@ -1,6 +1,9 @@
 package models
 
+import "time"
+
 type Session struct {
-	ID    string   `json:"id"`    // случайный session_id
-	Notes []string `json:"notes"` // список ID заметок, которые создал пользователь
+	ID        string    `json:"id"`         // Случайный session_id
+	Notes     []string  `json:"notes"`      // Список ID заметок, которые создал пользователь
+	ExpiresAt time.Time `json:"expires_at"` // Окончание сессии
 }
