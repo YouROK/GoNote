347b5a004910db3d98add1654708b9ab581d88b4
YouROK <8yourok8@mail.ru>
2025-09-13 12:13:28 +0300

    rem unused

--- models/page.go
+++ models/page.go
@@ -2,20 +2,11 @@ package models
 
 import "time"
 
-type NoteStatus string
-
-const (
-	StatusDraft     NoteStatus = "draft"
-	StatusPublished NoteStatus = "published"
-	StatusHidden    NoteStatus = "hidden"
-)
-
 type Note struct {
-	ID        string     `json:"id"`     // хэш страницы, уникальный
-	Author    string     `json:"author"` // uid или имя пользователя
-	Title     string     `json:"title"`
-	Password  string     `json:"password"` // bcrypt-хеш, если задан
-	Status    NoteStatus `json:"status"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
+	ID        string    `json:"id"` // хэш страницы, уникальный
+	Author    string    `json:"author"`
+	Title     string    `json:"title"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
