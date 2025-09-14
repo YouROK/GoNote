d01e6fe76c78918b95d37d2554a0c8bb5d7ed103
YouROK <8yourok8@mail.ru>
2025-09-14 21:31:58 +0300

    update

--- web/static/html.go
+++ web/static/html.go
@@ -4,83 +4,86 @@ import (
 	_ "embed"
 )
 
-//go:embed files/img/android-icon-144x144.png
-var Filesimgandroidicon144x144png []byte
+//go:embed files/android-icon-144x144.png
+var Filesandroidicon144x144png []byte
 
-//go:embed files/img/android-icon-192x192.png
-var Filesimgandroidicon192x192png []byte
+//go:embed files/android-icon-192x192.png
+var Filesandroidicon192x192png []byte
 
-//go:embed files/img/android-icon-36x36.png
-var Filesimgandroidicon36x36png []byte
+//go:embed files/android-icon-36x36.png
+var Filesandroidicon36x36png []byte
 
-//go:embed files/img/android-icon-48x48.png
-var Filesimgandroidicon48x48png []byte
+//go:embed files/android-icon-48x48.png
+var Filesandroidicon48x48png []byte
 
-//go:embed files/img/android-icon-72x72.png
-var Filesimgandroidicon72x72png []byte
+//go:embed files/android-icon-72x72.png
+var Filesandroidicon72x72png []byte
 
-//go:embed files/img/android-icon-96x96.png
-var Filesimgandroidicon96x96png []byte
+//go:embed files/android-icon-96x96.png
+var Filesandroidicon96x96png []byte
 
-//go:embed files/img/apple-icon-114x114.png
-var Filesimgappleicon114x114png []byte
+//go:embed files/apple-icon-114x114.png
+var Filesappleicon114x114png []byte
 
-//go:embed files/img/apple-icon-120x120.png
-var Filesimgappleicon120x120png []byte
+//go:embed files/apple-icon-120x120.png
+var Filesappleicon120x120png []byte
 
-//go:embed files/img/apple-icon-144x144.png
-var Filesimgappleicon144x144png []byte
+//go:embed files/apple-icon-144x144.png
+var Filesappleicon144x144png []byte
 
-//go:embed files/img/apple-icon-152x152.png
-var Filesimgappleicon152x152png []byte
+//go:embed files/apple-icon-152x152.png
+var Filesappleicon152x152png []byte
 
-//go:embed files/img/apple-icon-180x180.png
-var Filesimgappleicon180x180png []byte
+//go:embed files/apple-icon-180x180.png
+var Filesappleicon180x180png []byte
 
-//go:embed files/img/apple-icon-57x57.png
-var Filesimgappleicon57x57png []byte
+//go:embed files/apple-icon-57x57.png
+var Filesappleicon57x57png []byte
 
-//go:embed files/img/apple-icon-60x60.png
-var Filesimgappleicon60x60png []byte
+//go:embed files/apple-icon-60x60.png
+var Filesappleicon60x60png []byte
 
-//go:embed files/img/apple-icon-72x72.png
-var Filesimgappleicon72x72png []byte
+//go:embed files/apple-icon-72x72.png
+var Filesappleicon72x72png []byte
 
-//go:embed files/img/apple-icon-76x76.png
-var Filesimgappleicon76x76png []byte
+//go:embed files/apple-icon-76x76.png
+var Filesappleicon76x76png []byte
 
-//go:embed files/img/apple-icon-precomposed.png
-var Filesimgappleiconprecomposedpng []byte
+//go:embed files/apple-icon-precomposed.png
+var Filesappleiconprecomposedpng []byte
 
-//go:embed files/img/apple-icon.png
-var Filesimgappleiconpng []byte
+//go:embed files/apple-icon.png
+var Filesappleiconpng []byte
 
-//go:embed files/img/browserconfig.xml
-var Filesimgbrowserconfigxml []byte
+//go:embed files/browserconfig.xml
+var Filesbrowserconfigxml []byte
 
-//go:embed files/img/favicon-16x16.png
-var Filesimgfavicon16x16png []byte
+//go:embed files/favicon-16x16.png
+var Filesfavicon16x16png []byte
 
-//go:embed files/img/favicon-32x32.png
-var Filesimgfavicon32x32png []byte
+//go:embed files/favicon-32x32.png
+var Filesfavicon32x32png []byte
 
-//go:embed files/img/favicon-96x96.png
-var Filesimgfavicon96x96png []byte
+//go:embed files/favicon-96x96.png
+var Filesfavicon96x96png []byte
 
-//go:embed files/img/favicon.ico
-var Filesimgfaviconico []byte
+//go:embed files/favicon.ico
+var Filesfaviconico []byte
 
-//go:embed files/img/manifest.json
-var Filesimgmanifestjson []byte
+//go:embed files/manifest.json
+var Filesmanifestjson []byte
 
-//go:embed files/img/ms-icon-144x144.png
-var Filesimgmsicon144x144png []byte
+//go:embed files/ms-icon-144x144.png
+var Filesmsicon144x144png []byte
 
-//go:embed files/img/ms-icon-150x150.png
-var Filesimgmsicon150x150png []byte
+//go:embed files/ms-icon-150x150.png
+var Filesmsicon150x150png []byte
 
-//go:embed files/img/ms-icon-310x310.png
-var Filesimgmsicon310x310png []byte
+//go:embed files/ms-icon-310x310.png
+var Filesmsicon310x310png []byte
 
-//go:embed files/img/ms-icon-70x70.png
-var Filesimgmsicon70x70png []byte
+//go:embed files/ms-icon-70x70.png
+var Filesmsicon70x70png []byte
+
+//go:embed files/robots.txt
+var Filesrobotstxt []byte
--- web/static/route.go
+++ web/static/route.go
@@ -9,191 +9,198 @@ import (
 func RouteStaticFiles(route *gin.Engine) {
 
 	route.GET("/android-icon-144x144.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgandroidicon144x144png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesandroidicon144x144png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgandroidicon144x144png)
+		c.Data(200, "image/png", Filesandroidicon144x144png)
 	})
 
 	route.GET("/android-icon-192x192.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgandroidicon192x192png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesandroidicon192x192png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgandroidicon192x192png)
+		c.Data(200, "image/png", Filesandroidicon192x192png)
 	})
 
 	route.GET("/android-icon-36x36.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgandroidicon36x36png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesandroidicon36x36png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgandroidicon36x36png)
+		c.Data(200, "image/png", Filesandroidicon36x36png)
 	})
 
 	route.GET("/android-icon-48x48.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgandroidicon48x48png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesandroidicon48x48png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgandroidicon48x48png)
+		c.Data(200, "image/png", Filesandroidicon48x48png)
 	})
 
 	route.GET("/android-icon-72x72.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgandroidicon72x72png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesandroidicon72x72png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgandroidicon72x72png)
+		c.Data(200, "image/png", Filesandroidicon72x72png)
 	})
 
 	route.GET("/android-icon-96x96.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgandroidicon96x96png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesandroidicon96x96png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgandroidicon96x96png)
+		c.Data(200, "image/png", Filesandroidicon96x96png)
 	})
 
 	route.GET("/apple-icon-114x114.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleicon114x114png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleicon114x114png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleicon114x114png)
+		c.Data(200, "image/png", Filesappleicon114x114png)
 	})
 
 	route.GET("/apple-icon-120x120.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleicon120x120png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleicon120x120png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleicon120x120png)
+		c.Data(200, "image/png", Filesappleicon120x120png)
 	})
 
 	route.GET("/apple-icon-144x144.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleicon144x144png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleicon144x144png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleicon144x144png)
+		c.Data(200, "image/png", Filesappleicon144x144png)
 	})
 
 	route.GET("/apple-icon-152x152.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleicon152x152png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleicon152x152png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleicon152x152png)
+		c.Data(200, "image/png", Filesappleicon152x152png)
 	})
 
 	route.GET("/apple-icon-180x180.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleicon180x180png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleicon180x180png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleicon180x180png)
+		c.Data(200, "image/png", Filesappleicon180x180png)
 	})
 
 	route.GET("/apple-icon-57x57.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleicon57x57png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleicon57x57png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleicon57x57png)
+		c.Data(200, "image/png", Filesappleicon57x57png)
 	})
 
 	route.GET("/apple-icon-60x60.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleicon60x60png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleicon60x60png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleicon60x60png)
+		c.Data(200, "image/png", Filesappleicon60x60png)
 	})
 
 	route.GET("/apple-icon-72x72.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleicon72x72png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleicon72x72png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleicon72x72png)
+		c.Data(200, "image/png", Filesappleicon72x72png)
 	})
 
 	route.GET("/apple-icon-76x76.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleicon76x76png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleicon76x76png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleicon76x76png)
+		c.Data(200, "image/png", Filesappleicon76x76png)
 	})
 
 	route.GET("/apple-icon-precomposed.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleiconprecomposedpng))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleiconprecomposedpng))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleiconprecomposedpng)
+		c.Data(200, "image/png", Filesappleiconprecomposedpng)
 	})
 
 	route.GET("/apple-icon.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgappleiconpng))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesappleiconpng))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgappleiconpng)
+		c.Data(200, "image/png", Filesappleiconpng)
 	})
 
 	route.GET("/browserconfig.xml", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgbrowserconfigxml))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesbrowserconfigxml))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "text/xml; charset=utf-8", Filesimgbrowserconfigxml)
+		c.Data(200, "text/xml; charset=utf-8", Filesbrowserconfigxml)
 	})
 
 	route.GET("/favicon-16x16.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgfavicon16x16png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesfavicon16x16png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgfavicon16x16png)
+		c.Data(200, "image/png", Filesfavicon16x16png)
 	})
 
 	route.GET("/favicon-32x32.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgfavicon32x32png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesfavicon32x32png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgfavicon32x32png)
+		c.Data(200, "image/png", Filesfavicon32x32png)
 	})
 
 	route.GET("/favicon-96x96.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgfavicon96x96png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesfavicon96x96png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgfavicon96x96png)
+		c.Data(200, "image/png", Filesfavicon96x96png)
 	})
 
 	route.GET("/favicon.ico", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgfaviconico))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesfaviconico))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/vnd.microsoft.icon", Filesimgfaviconico)
+		c.Data(200, "image/vnd.microsoft.icon", Filesfaviconico)
 	})
 
 	route.GET("/manifest.json", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgmanifestjson))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesmanifestjson))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "application/json", Filesimgmanifestjson)
+		c.Data(200, "application/json", Filesmanifestjson)
 	})
 
 	route.GET("/ms-icon-144x144.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgmsicon144x144png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesmsicon144x144png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgmsicon144x144png)
+		c.Data(200, "image/png", Filesmsicon144x144png)
 	})
 
 	route.GET("/ms-icon-150x150.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgmsicon150x150png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesmsicon150x150png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgmsicon150x150png)
+		c.Data(200, "image/png", Filesmsicon150x150png)
 	})
 
 	route.GET("/ms-icon-310x310.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgmsicon310x310png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesmsicon310x310png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgmsicon310x310png)
+		c.Data(200, "image/png", Filesmsicon310x310png)
 	})
 
 	route.GET("/ms-icon-70x70.png", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Filesimgmsicon70x70png))
+		etag := fmt.Sprintf("%x", md5.Sum(Filesmsicon70x70png))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "image/png", Filesimgmsicon70x70png)
+		c.Data(200, "image/png", Filesmsicon70x70png)
+	})
+
+	route.GET("/robots.txt", func(c *gin.Context) {
+		etag := fmt.Sprintf("%x", md5.Sum(Filesrobotstxt))
+		c.Header("Cache-Control", "public, max-age=31536000")
+		c.Header("ETag", etag)
+		c.Data(200, "text/plain; charset=utf-8", Filesrobotstxt)
 	})
 }
